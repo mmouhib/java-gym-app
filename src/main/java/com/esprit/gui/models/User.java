72e589d73f3b39e1fdd5ba3f96f5cfaab06b2a1b
YHbibi <hbibiyoussef27@gmail.com>
2024-02-10 17:09:13 +0100

    add some user's attributs

--- src/main/java/com/esprit/gui/models/User.java
+++ src/main/java/com/esprit/gui/models/User.java
@@ -9,8 +9,12 @@ import lombok.ToString;
 @ToString
 public class User {
     private int id;
-    private String name;
+    private String first_name;
+    private String last_name;
+    private String phone;
+    private String email;
     private int age;
+    private String gender;
     private float weight;
     private float height;
     private float calories;
@@ -19,12 +23,16 @@ public class User {
     private float fat;
     private float sugar;
     private float targetWeight;
+    private String role;
 
-
-    public User(int id, String name, int age, float weight, float height, float calories, float protein, float carbs, float fat, float sugar, float targetWeight) {
+    public User(int id, String first_name, String last_name, String phone, String email, int age, String gender, float weight, float height, float calories, float protein, float carbs, float fat, float sugar, float targetWeight, String role) {
         this.id = id;
-        this.name = name;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone = phone;
+        this.email = email;
         this.age = age;
+        this.gender = gender;
         this.weight = weight;
         this.height = height;
         this.calories = calories;
@@ -33,10 +41,12 @@ public class User {
         this.fat = fat;
         this.sugar = sugar;
         this.targetWeight = targetWeight;
+        this.role = role;
     }
 
-    public User(String name, int age, float weight, float height, float calories, float protein, float carbs, float fat, float sugar, float targetWeight) {
-        this.name = name;
+    public User(String first_name, String last_name, int age, float weight, float height, float calories, float protein, float carbs, float fat, float sugar, float targetWeight) {
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.age = age;
         this.weight = weight;
         this.height = height;
