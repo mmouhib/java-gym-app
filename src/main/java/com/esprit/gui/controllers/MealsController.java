c0d33beccfdd93bbf420d5f35d62a884171803fd
mmouhib <iwritebadcode@github.com>
2024-04-06 04:47:06 +0100

    fix: fix meals `tableview` component

--- src/main/java/com/esprit/gui/controllers/MealsController.java
+++ src/main/java/com/esprit/gui/controllers/MealsController.java
@@ -1,8 +1,14 @@
 package com.esprit.gui.controllers;
 
 import com.esprit.gui.models.Meal;
+import com.esprit.gui.models.Plate;
+import com.esprit.gui.models.tableviewmodels.MealsModel;
+import com.esprit.gui.models.tableviewmodels.PlatesModel;
 import com.esprit.gui.repository.MealRepository;
 import com.esprit.gui.repository.PlatesRepository;
+import com.esprit.gui.utils.AuthSessionUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,26 +16,29 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.DragEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.sql.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MealsController implements Initializable {
 
 
     public Button home;
-    public ComboBox<String> type;
+    public ComboBox<String> type_input;
     public CheckBox isToday;
     public DatePicker dateInput;
     public Text quantityValue;
-    public Slider quantity;
+    public Slider quantity_input;
     public Button resetButton;
     public Button saveButton;
     public TextField name_input;
@@ -41,9 +50,21 @@ public class MealsController implements Initializable {
     public Button nutrition;
     public ComboBox<String> category;
 
+
+    public TableView<MealsModel> mealsTableView;
+    public TableColumn<MealsModel, String> name;
+    public TableColumn<MealsModel, Integer> quantity;
+    public TableColumn<MealsModel, String> date;
+    public TableColumn<MealsModel, String> plate;
+    public TableColumn<MealsModel, String> type;
+    public TableColumn<MealsModel, Integer> id;
+    public Button deleteButton;
+    private ObservableList<MealsModel> mealsModels = FXCollections.observableArrayList();
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        quantity.valueProperty().addListener((observable, oldValue, newValue) -> {
+        quantity_input.valueProperty().addListener((observable, oldValue, newValue) -> {
             quantityValue.setText(Integer.toString(newValue.intValue()));
         });
         category.getItems().add("breakfast");
@@ -51,17 +72,46 @@ public class MealsController implements Initializable {
         category.getItems().add("dinner");
         category.getItems().add("snack");
         updatePlatesDropdown();
+        initializeTableView();
+    }
+
+    private void initializeTableView() {
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        plate.setCellValueFactory(new PropertyValueFactory<>("plate"));
+        type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        addToTableView();
+
+    }
+
+    private void addToTableView() {
+        mealsModels.clear();
+        List<Meal> meals = new MealRepository().getMealsByUser(Integer.parseInt(AuthSessionUtils.getCurrentUser()));
+        for (Meal meal : meals) {
+            String plateName = new PlatesRepository().findById(meal.getPlateId()).getName();
+            mealsModels.add(new MealsModel(
+                    meal.getId(),
+                    meal.getName(),
+                    meal.getQuantity(),
+                    meal.getDate().toString(),
+                    plateName,
+                    meal.getCategory()
+            ));
+        }
+        mealsTableView.setItems(mealsModels);
     }
 
 
     private void updatePlatesDropdown() {
         PlatesRepository platesRepository = new PlatesRepository();
         platesRepository.list().forEach(plate -> {
-            type.getItems().add(plate.getId() + " - " + plate.getName());
+            type_input.getItems().add(plate.getId() + " - " + plate.getName());
         });
     }
 
-    public void updateDateInput(DragEvent actionEvent) {
+    public void updateDateInput() {
         dateInput.setDisable(isToday.isSelected());
         dateInput.setValue(LocalDate.parse(new Date(Calendar.getInstance().getTimeInMillis()).toString()));
     }
@@ -69,23 +119,46 @@ public class MealsController implements Initializable {
     public void updateQuantityValue() {
     }
 
+    private Date localDateToDate() {
+        String inputDate = dateInput.getValue().toString();
+        Date result = new Date(
+                Integer.parseInt(inputDate.split("-")[0]) - 1900,
+                Integer.parseInt(inputDate.split("-")[1]) - 1,
+                Integer.parseInt(inputDate.split("-")[2])
+        );
+        return result;
+    }
+
     public void onSave(ActionEvent actionEvent) {
+        mealsModels.clear();
         MealRepository mealRepository = new MealRepository();
         mealRepository.save(
                 new Meal(
                         name_input.getText(),
-                        Date.valueOf(dateInput.getValue()),
+                        localDateToDate(),
                         Integer.parseInt(quantityValue.getText()),
-                        // Integer.parseInt(AuthSessionUtils.getCurrentUser()),
                         category.getValue(),
-                        4,
-                        Integer.parseInt(type.getValue().split(" - ")[0])
+                        Integer.parseInt(AuthSessionUtils.getCurrentUser()),
+                        Integer.parseInt(type_input.getValue().split(" - ")[0])
                 )
         );
+        addToTableView();
 
     }
 
+
+    public void onDelete(ActionEvent actionEvent) {
+        mealsTableView.getSelectionModel().getSelectedItems().forEach(mealsModel -> {
+            new MealRepository().delete(mealsModel.getId());
+        });
+        addToTableView();
+    }
+
     public void clearForm(ActionEvent actionEvent) {
+        name_input.setText("");
+        quantity_input.setValue(0);
+        type_input.setValue("");
+        category.setValue("");
     }
 
 
--- src/main/java/com/esprit/gui/models/tableviewmodels/MealsModel.java
+++ src/main/java/com/esprit/gui/models/tableviewmodels/MealsModel.java
@@ -1,4 +1,96 @@
 package com.esprit.gui.models.tableviewmodels;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class MealsModel {
+
+
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty name;
+    private SimpleIntegerProperty quantity;
+    private SimpleStringProperty date;
+    private SimpleStringProperty plate;
+    private SimpleStringProperty type;
+
+    public MealsModel(int id, String name, int quantity, String date, String plate, String type) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.quantity = new SimpleIntegerProperty(quantity);
+        this.date = new SimpleStringProperty(date);
+        this.plate = new SimpleStringProperty(plate);
+        this.type = new SimpleStringProperty(type);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public int getQuantity() {
+        return quantity.get();
+    }
+
+    public SimpleIntegerProperty quantityProperty() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity.set(quantity);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+
+    public String getPlate() {
+        return plate.get();
+    }
+
+    public SimpleStringProperty plateProperty() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate.set(plate);
+    }
+
+    public String getType() {
+        return type.get();
+    }
+
+    public SimpleStringProperty typeProperty() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type.set(type);
+    }
 }
